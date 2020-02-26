633029a1095d706bf74b4b5c0a979a9138e39f81
Kyle Talmage <ktalmage85@gmail.com>
2020-02-26 22:19:42 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,7 @@
 class Person
-  attr_accessor :name, :birthday
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
+:weight, :handed, :complexion, :t_shirt_size, 
+:wrist_size, :glove_size, :pant_length, :pant_width
   
   attributes = {name: "Bob", birthday: "12/11/1980"}
   
