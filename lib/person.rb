b51bf4f063cc2231e27bb19c83b975d289a93c30
Kyle Talmage <ktalmage85@gmail.com>
2020-02-26 22:14:57 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,5 +4,6 @@ class Person
   attributes = { name: "Bob", birthday: "March 10th", height: "5'3", weight: "100",eye_color: "brown"}
   
   def initialize(attributes)
-    
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
 end
