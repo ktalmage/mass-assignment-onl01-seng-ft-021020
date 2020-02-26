cbc28df32db77f24ec48dd2217db27c724391268
Kyle Talmage <ktalmage85@gmail.com>
2020-02-26 22:14:34 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,8 @@
 class Person
-  #your code here
+  attr_accessor :name, :birthday,:height, :weight, :eye_color
+  
+  attributes = { name: "Bob", birthday: "March 10th", height: "5'3", weight: "100",eye_color: "brown"}
+  
+  def initialize(attributes)
+    
 end
